5cc246ebcdd8695a44b8412c5fb320ab5e7f6c79
dvdcrc89@gmail.com <dvdcrc89@gmail.com>
2019-02-24 20:01:04 +0000

    Implements Browser animation

--- js/game/prefabs/Browser.js
+++ js/game/prefabs/Browser.js
@@ -3,8 +3,8 @@ Browser = function(game) {
     Phaser.Sprite.call(this, game, 50, 100, "browser");
     game.add.existing(this);
     this.scale.setTo(0.3);
-    // this.animations.add("run",[1,2,3,0]);
-    // this.animations.add("jump",[4,4,0]);
+    this.animations.add("run");
+    this.animations.play("run",5,true);
     game.physics.enable([ this ], Phaser.Physics.ARCADE);
 
     this.body.collideWorldBounds = true;
