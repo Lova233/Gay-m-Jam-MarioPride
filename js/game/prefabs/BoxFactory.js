cbab7a3f859fa95e873abf98ecc2fb11263d7e78
dvdcrc89@gmail.com <dvdcrc89@gmail.com>
2019-02-24 18:05:38 +0000

    Implements alghortims for box creation

--- js/game/prefabs/BoxFactory.js
+++ js/game/prefabs/BoxFactory.js
@@ -3,12 +3,14 @@ BoxFactory = function(game) {
     this.boxes = game.add.group();
     this.lastX;
     this.lastY;
+    this.lastBoxTime = game.time.now;
+    this.boxRecall = 2800;
     this.addBox= function(x,y){
         let box = game.add.sprite(x, y, 'square');
         box.scale.setTo(0.2);
         game.physics.arcade.enable([box]);
-        box.body.velocity.x = -80;
-
+        box.body.velocity.x = -130;
+        box.body.immovable = true;
         box.body.allowGravity = false;
         this.lastX = box.x;
         this.lastY = box.y;
@@ -16,9 +18,19 @@ BoxFactory = function(game) {
     }
 
     this.startFactory = function(){
+     if(game.time.now>this.lastBoxTime+this.boxRecall)   {
+        let numberOfBox = Math.floor(Math.random()*4)+1;
+    console.log(numberOfBox);
+     for(let i =0;i<numberOfBox;i++){   
+        this.addBox(game.width+i*50,500-i*100);
+        let lengthOfRow = Math.floor(Math.random()*5);
+         for(let a =0;a<lengthOfRow;a++){
+            this.addBox(game.width+i*50+a*50,500-i*100);
 
-        if(Math.random()*10>8)
-            this.addBox(game.width,500);
+        } 
+     }
+     this.lastBoxTime = game.time.now;
+     }
     }
 }
 BoxFactory.prototype = Object.create(Phaser.Sprite.prototype);
